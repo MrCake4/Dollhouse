aab3f039e4d9e8003e7230493666f642f108b9e3
Jeswee <Jessica.Wiecha@Student.HTW-Berlin.de>
2025-04-21 18:53:31 +0200

    PlayerMovement
    walking bean in moderate speed

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+
+    //public float moveSpeed = 7f;                      //public, damit man es im Editor sieht/ ändern kann (Inspector) --> kann man dann auch während runtime ändern  !!Nachteil: andere  Klassen haben Zugriff & können Variable verändern
+    [SerializeField] private float moveSpeed = 7f;      //Alternativ mit [SerializeField] --> jetzt  private + gleichzeitig für den Editor sichtbar
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,10 +34,14 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("Space");
 
         }
+
+    //FIRST GET INPUT -- THEN ACTUALLY MOVE THE OBJECT
+
         inputVector = inputVector.normalized;               // Normalisieren (eigentlich eher wichtig, wenn man zwei Vektoren addiert --> hier aber nur rechts oder links bisher --> also egal)
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += (Vector3)inputVector;         // zu 3D Vektor casten für die Spielwelt
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);             //keep input Vector separate fro the movement Vector
+        transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;         // zu 3D Vektor casten für die Spielwelt
+        //damit speed vom Player nicht abhängig von der Frame Rate ist --> multiply by (delta) time   ==> weil das alleine ultra langsam ist --> *moveSpeed
 
 
         // Debug.Log(Vector2.zero);
