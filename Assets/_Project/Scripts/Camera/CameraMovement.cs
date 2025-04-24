1794d07aa55c1dae904bcac0d3ffae8123369d42
Winston Reichelt <winstonjreichelt@gmail.com>
2025-04-24 12:28:30 +0200

    added basic side scroller room based transitions

--- Assets/_Project/Scripts/Camera/CameraMovement.cs
+++ Assets/_Project/Scripts/Camera/CameraMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,17 +9,37 @@ public class CameraMovement : MonoBehaviour
     [Header("Camera Settings")]
     [SerializeField] Vector3 cameraPosition;
     [SerializeField] Transform player;
+    private Vector3 playerPosition;
+
+    [Header("Room to room behaviour")]
+    [SerializeField] Room[] rooms;
+    [SerializeField] Room currentRoom;
+    [SerializeField] Vector3 targetCameraPos;
+    [SerializeField] float cameraSpeed = 3f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        targetCameraPos = currentRoom.cameraAnchorPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerPosition = player.transform.position;
+
+        foreach(Room room in rooms){
+            if(room.roomCollider.bounds.Contains(playerPosition)){
+                if (room != currentRoom){
+                    currentRoom = room;
+                    targetCameraPos = room.cameraAnchorPoint.position;
+                    Debug.Log("Room changed");
+                }
+            }
+        }
         // maybe transform.lookAt(Vector3) benutzen
-        transform.position = player.transform.position + cameraPosition;
+
+        // Camera stays at player position and adds it by the camera position
+        transform.position = Vector3.Lerp(transform.position, targetCameraPos + cameraPosition, Time.deltaTime * cameraSpeed);
     }
 }
---
+++ Assets/_Project/Scripts/Camera/Room.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Collider roomCollider;
+    public Transform cameraAnchorPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
