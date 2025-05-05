547202928d1e07050ecc993307af0464ecc11e0b
Winston Reichelt <winstonjreichelt@gmail.com>
2025-05-05 21:38:01 +0200

    refactored code

--- Assets/_Project/Scripts/Particles/DynamicDust.cs
+++ Assets/_Project/Scripts/Particles/DynamicDust.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class DynamicDust : MonoBehaviour
 {
+    /*
+    *   Inspired by the Dust Particles used in little nightmares 2
+    *   This script modifies the velocity of particles in a ParticleSystem
+    */
+
+    [Header("Dynamic Dust Settings")]
     public ParticleSystem particleSystem;
     public Transform player;
-    public float repelRadius = 2f;
-    public float repelForce = 1f;
+    [Tooltip("The radius within which particles are repelled from the player"), Range(0f, 2f)]
+    public float repelRadius = 2f;  // radius within which particles are repelled
+    [Tooltip("The force applied to repel particles from the player"), Range(0f, 2f)]
+    public float repelForce = 1f;   // force applied to repel particles
 
-    private ParticleSystem.Particle[] particles;
+    private ParticleSystem.Particle[] particles;        // stores particles in an array
 
+    // LateUpdate is called after all Update functions have been called
+    // This is where we will modify the particles' velocities
     void LateUpdate()
     {
         if (particles == null || particles.Length < particleSystem.main.maxParticles)
