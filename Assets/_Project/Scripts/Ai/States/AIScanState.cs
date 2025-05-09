e1f91df306d3f77a7c5ddd54d2ec7f144379459b
P1exes <le.maxanh@gmx.de>
2025-05-09 17:29:58 +0200

    Some changes

--- Assets/_Project/Scripts/Ai/States/AIScanState.cs
+++ Assets/_Project/Scripts/Ai/States/AIScanState.cs
@@ -3,22 +3,45 @@ using UnityEngine.Animations;
 
 public class AIScanState : AIBaseState
 {
+    RoomContainer currentTargetRoom;
     float checkRoomTime;
     public override void enterState(AIStateManager ai){
         Debug.Log("Dolly entered Scan State");
         checkRoomTime = ai.getCheckRoomTime;
+        currentTargetRoom = ai.currentTargetRoom;
+
         ai.eye.setStartScan(true);
 
+        // Sets the first window as target
+        ai.setCurrentTargetWindow(currentTargetRoom.windowAnchorPoints[ai.currentWindowIndex]);
     }
     
     public override void onUpdate(AIStateManager ai){
 
         Debug.Log("Scan Window");
+
         checkRoomTime -= Time.deltaTime;
-        // if eye sees player
-        //      ai.switchState(ai.attackState)
         if (checkRoomTime <= 0.0f) {
-            exitState(ai);}
+            exitState(ai);
+        }
+
+        if (currentTargetRoom == null || currentTargetRoom.windowAnchorPoints.Length == 0) {
+            ai.switchState(ai.patrolState, false);
+            return;
+        }
+
+         // If reached current window, move to next
+        if (Vector3.Distance(ai.transform.position, ai.currentTargetWindow.position) < 0.1f) {
+            
+            if(ai.currentWindowIndex <= currentTargetRoom.windowCount - 1) {
+                ai.currentWindowIndex++;
+            } else {
+                ai.seekIncrement = 0;        
+                ai.currentWindowIndex = 0; // Reset window index for next room
+                ai.switchState(ai.seekState, false); // Go to next room
+            }
+             // Move toward current window
+        } else {ai.transform.position = Vector3.MoveTowards(ai.transform.position,ai.currentTargetWindow.position,Time.deltaTime * ai.moveSpeed);}
     }
 
     public override void resetVariables(AIStateManager ai){}
