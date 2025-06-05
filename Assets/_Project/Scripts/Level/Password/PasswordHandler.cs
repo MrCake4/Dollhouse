3919525ca3af23d667a1cc53e40a52f37235497b
Winston Reichelt <winstonjreichelt@gmail.com>
2025-06-05 23:04:08 +0200

    Password Keypad is working needs some major adjustements though

--- Assets/_Project/Scripts/Level/Password/PasswordHandler.cs
+++ Assets/_Project/Scripts/Level/Password/PasswordHandler.cs
@@ -5,6 +5,7 @@ public class PasswordHandler : MonoBehaviour
 {
     int password;   // a pasword variable to store the generated password
     [SerializeField] Keypad keypad; // reference to the Keypad component
+    [SerializeField] NumberRenderer[] numberQuads; // array of NumberRenderer components to display the digits
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +15,18 @@ public class PasswordHandler : MonoBehaviour
         Debug.Log("Generated Password: " + password);
 
         keypad.setKeyPadCombo(password); // Set the generated password in the Keypad component
+        setNumberQuads(password); // Set the password digits in the NumberRenderer components
+    }
+
+    void setNumberQuads(int password)
+    {
+        string passwordStr = password.ToString();
+
+        // Extract digits
+        // int firstDigit = int.Parse(passwordStr[0].ToString());
+        for (int i = 0; i < numberQuads.Length; i++)
+        {
+            numberQuads[i].UpdateUVs(int.Parse(passwordStr[i].ToString()));
+        }
     }
 }
