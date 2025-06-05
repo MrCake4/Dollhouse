447ac666f13ca360200a6e22ee39e778653e2440
Winston Reichelt <winstonjreichelt@gmail.com>
2025-06-05 20:52:12 +0200

    added a script for an already lit candle

--- Assets/_Project/Scripts/Level/Candles/Candle.cs
+++ Assets/_Project/Scripts/Level/Candles/Candle.cs
@@ -1,29 +1,62 @@
 using UnityEngine;
 
+// Requieres PlayerItemHandler.cs to be attached to the player
+// Requieres a Light component as child of the candle object
+
 public class Candle : Interactable
 {
     PlayerItemHandler playerItemHandler;
     bool isLit = false;
     Light candleLight;
+    float minIntensity;
+    float maxIntensity;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float currentTargetIntensity;
+    float lerpSpeed = 2f; // How fast the light flickers
+    float nextTargetTime;
+    float targetChangeInterval = 0.5f; // How often to change the intensity target
+
     void Start()
     {
         playerItemHandler = FindFirstObjectByType<PlayerItemHandler>();
         candleLight = GetComponentInChildren<Light>();
+
         if (candleLight != null)
         {
-            candleLight.enabled = false; // Start with the light off
+            candleLight.enabled = false;
+            minIntensity = candleLight.intensity * 0.5f;
+            maxIntensity = candleLight.intensity * 1.5f;
+            currentTargetIntensity = candleLight.intensity;
         }
     }
 
     public override void interact()
     {
-        // if playeritemhandler.getCarriedObject() is candle turn on light
-        if (playerItemHandler.GetCarriedObject() != null && playerItemHandler.GetCarriedObject().CompareTag("Candle") && !isLit)
+        // if the player is carrying a candle and it is not lit, light it
+        if (playerItemHandler.GetCarriedObject() != null &&
+            playerItemHandler.GetCarriedObject().CompareTag("Candle") &&
+            !isLit)
         {
             isLit = true;
-            candleLight.enabled = true; // Turn on the light
+            candleLight.enabled = true;
+            currentTargetIntensity = Random.Range(minIntensity, maxIntensity);
+            nextTargetTime = Time.time + targetChangeInterval;
+        }
+    }
+
+    void Update()
+    {
+        if (isLit && candleLight != null)
+        {
+            // Smoothly interpolate intensity
+            candleLight.intensity = Mathf.Lerp(candleLight.intensity, currentTargetIntensity, Time.deltaTime * lerpSpeed);
+
+            // Change target intensity periodically
+            if (Time.time >= nextTargetTime)
+            {
+                currentTargetIntensity = Random.Range(minIntensity, maxIntensity);
+                nextTargetTime = Time.time + targetChangeInterval;
+            }
         }
     }
 }
