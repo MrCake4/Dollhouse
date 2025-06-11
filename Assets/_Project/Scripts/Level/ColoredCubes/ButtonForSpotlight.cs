1988d1dfa03455fb0b32124bf29be4f667c3a802
Winston Reichelt <winstonjreichelt@gmail.com>
2025-06-11 15:07:43 +0200

    new classes and did some adjustments on the interactable class

--- Assets/_Project/Scripts/Level/ColoredCubes/ButtonForSpotlight.cs
+++ Assets/_Project/Scripts/Level/ColoredCubes/ButtonForSpotlight.cs
@@ -30,4 +30,12 @@ public class ButtonForSpotlight : Interactable
     {
         LightSwitch();
     }
+
+    public override void onPowerOn()
+    {
+    }
+
+    public override void onPowerOff()
+    {
+    }
 }
---
+++ Assets/_Project/Scripts/Level/Generator/HitableObject.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class HitableObject : MonoBehaviour
+{
+    public abstract void onHit();
+}
