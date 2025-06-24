d1a6f0a4e97c372dafae72c795e8a559ea41bd5c
Jeswee <Jessica.Wiecha@Student.HTW-Berlin.de>
2025-06-24 11:49:24 +0200

    better Statemachin with weird jump

--- Assets/_Project/Scripts/Player/Animation_Scripts/PlayerAnimation.cs
+++ Assets/_Project/Scripts/Player/Animation_Scripts/PlayerAnimation.cs
@@ -5,10 +5,9 @@ public class PlayerAnimation : MonoBehaviour
     private PlayerStateManager player;
     private Animator animator;
 
-
     [Header("Animator Parameters")]
     public string speedParam = "Speed";
-    
+    public string verticalVelocityParam = "VerticalVelocity"; // <- NEU
 
     void Start()
     {
@@ -18,11 +17,13 @@ public class PlayerAnimation : MonoBehaviour
 
     void Update()
     {
+        // Horizontale Bewegung (für Walk/Run/Idle)
         Vector3 horizontalVel = player.GetHorizontalVelocity();
         float speed = horizontalVel.magnitude;
-
         animator.SetFloat(speedParam, speed, 0.1f, Time.deltaTime);
 
-        
+        // Vertikale Bewegung (für Jump-Fall-BlendTree)
+        float verticalVelocity = player.rb.linearVelocity.y;
+        animator.SetFloat(verticalVelocityParam, verticalVelocity, 0.1f, Time.deltaTime);
     }
 }
