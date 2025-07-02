61ad93b2d83a8c910f7b1af20ffb477838180219
Winston Reichelt <winstonjreichelt@gmail.com>
2025-07-02 11:30:08 +0200

    renamed a script to fix a warning

--- Assets/_Project/Scripts/Audio/AudioListenerOrientation.cs
+++ Assets/_Project/Scripts/Audio/AudioListenerOrientation.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 // This Script just makes the Audio Listener always face the global forward direction.
 
 [RequireComponent(typeof(AudioListener))]
-public class AudioListener : MonoBehaviour
+public class AudioListenerOrientation : MonoBehaviour
 {
     void Update()
     {
