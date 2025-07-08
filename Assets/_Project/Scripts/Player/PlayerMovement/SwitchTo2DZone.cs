daac84f4a8cc606a777824a449ffbe0af773ec6e
Winston Reichelt <winstonjreichelt@gmail.com>
2025-07-08 13:47:15 +0200

    bug fixing

--- Assets/_Project/Scripts/Player/PlayerMovement/SwitchTo2DZone.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement/SwitchTo2DZone.cs
@@ -9,6 +9,19 @@ public class SwitchTo2DZone : MonoBehaviour
             other.GetComponent<PlayerStateManager>().is2DMode = true;
             Debug.Log("2.5D now!");
         }
+
+        // If other Object is *layer* "bigObject", find all game objects in children with tag "disableIn2D" and disable then
+        // big object layer
+        if (other.gameObject.layer == LayerMask.NameToLayer("bigObject"))
+        {
+            foreach (Transform child in other.transform)
+            {
+                if (child.CompareTag("disableIn2D"))
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -17,6 +30,18 @@ public class SwitchTo2DZone : MonoBehaviour
         {
             other.GetComponent<PlayerStateManager>().is2DMode = false;
             Debug.Log("back to 3D now!");
+        }
+
+        // If other Object is tag "bigObject", find all game objects in children with tag "disableIn2D" and enable them
+        if (other.gameObject.layer == LayerMask.NameToLayer("bigObject"))
+        {
+            foreach (Transform child in other.transform)
+            {
+                if (child.CompareTag("disableIn2D"))
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
