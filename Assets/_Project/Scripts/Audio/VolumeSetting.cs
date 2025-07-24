609fc89d8aeaa6d88d35f941431cf7ccdd9b8fd4
Winston Reichelt <winstonjreichelt@gmail.com>
2025-07-24 21:18:50 +0200

    fixed volume slider

--- Assets/_Project/Scripts/Audio/VolumeSetting.cs
+++ Assets/_Project/Scripts/Audio/VolumeSetting.cs
@@ -10,10 +10,32 @@ public class VolumeSetting : MonoBehaviour
     void Awake()
     {
         masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
+
 
     void SetMasterVolume(float value)
     {
         audioMixer.SetFloat("Master", Mathf.Log10(Mathf.Max(value, 0.0001f)) * 20);
+        Save();
+    }
+
+    void Load()
+    {
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", masterVolumeSlider.value);
     }
 }
